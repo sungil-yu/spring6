f7cbb9fc90c44367fe18454ffb502bdb41b16e24
ching <chinggin@naver.com>
2024-06-30 16:21:18 +0900

    init

---
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -0,0 +1,23 @@
+package tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+}
---
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring;
+
+//결제 준비 기능
+public class PaymentService {
+
+
+    public Payment prepare() {
+
+        return null;
+    }
+
+
+}
