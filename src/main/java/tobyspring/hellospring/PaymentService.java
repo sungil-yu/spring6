700d474e80e98ef231e9c00390ed7a755f200432
ching <chinggin@naver.com>
2024-07-01 12:30:18 +0900

    SoC basic

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -18,27 +18,27 @@ public class PaymentService {
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws
         IOException {
+        BigDecimal exRate = getExRate(currency); //원화 활율
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
         String apiKey = "f05a147b2d7951e5be05b02a";
         String apiUrl = "https://v6.exchangerate-api.com/v6/";
 
         URL url = new URL(apiUrl + apiKey + "/latest/" + currency);
-
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String res = br.lines().collect(Collectors.joining());
         br.close();
 
         ObjectMapper objectMapper = new ObjectMapper();
         ExchangeRateData data = objectMapper.readValue(res, ExchangeRateData.class);
-
         BigDecimal exRate = data.conversionRates().get("KRW");
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
